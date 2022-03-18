33db0243d5d71aedfa64b793662783de0eeea443
Tom Ross <tom@umpox.com>
2022-03-18 15:04:38 +0000

    feat: Add smoke tests

--- packages/web-functionality/jest.config.js
+++ packages/web-functionality/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'jest-puppeteer',
+  haste: {
+    providesModuleNodeModules: ['.*'],
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
 }
