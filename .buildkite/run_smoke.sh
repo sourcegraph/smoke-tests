280985c646605b225e5d17859e7c074fe1ec639d
Filip Haftek <filip.haftek@sourcegraph.com>
2022-04-07 10:28:01 +0200

    Fix env

--- .buildkite/run_smoke.sh
+++ .buildkite/run_smoke.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-SOURCEGRAPH_URL="$BASE_URL"
-echo "--- :fire: Running smoke tests against $SOURCEGRAPH_URL"
+echo "--- :fire: Running smoke tests against $BASE_URL"
 npx @sourcegraph/web-smoke-tests@latest start
